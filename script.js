422bb48e4b88299823a3723a842205817f61a43d
Michał Garnys <m.programista@wp.pl>
2025-03-06 22:17:49 +0100

    dodanie zegarka

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 function checkTime(i) {
-    if (i < 10) {i = "0" + i};
-    return i;
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
 }
 
 window.addEventListener("load", function clock() {
-    console.log("work")
-    let clockH1 = document.getElementById("zegar");
-    let time = new Date();
-    let hours = time.getHours();
-    let minutes = time.getMinutes();
-    let seconds = time.getSeconds();
-    hours = checkTime(hours);
-    minutes = checkTime(minutes);
-    seconds = checkTime(seconds)
-    clockH1.innerText = hours + ":" + minutes + ":" + seconds;
-    setTimeout(clock, 1000);
-    
-})
+  console.log("work");
+  let clockH1 = document.getElementById("zegar");
+  let time = new Date();
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
+  let seconds = time.getSeconds();
+  hours = checkTime(hours);
+  minutes = checkTime(minutes);
+  seconds = checkTime(seconds);
+  clockH1.innerText = hours + ":" + minutes + ":" + seconds;
+  setTimeout(clock, 1000);
+});
