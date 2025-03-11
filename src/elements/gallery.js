0eee3adb1aff9c4d1224edbbcbb2100cb276ba01
Michał Garnys <m.programista@wp.pl>
2025-03-11 22:59:27 +0100

    dodanie slidera

--- src/elements/gallery.js
+++ src/elements/gallery.js
@@ -1,14 +1,36 @@
 import "../styles/gallery.css";
+import { Splide, SplideSlide } from '@splidejs/react-splide';// Importujemy Splide i SplideSlide
+import "@splidejs/splide/dist/css/splide.min.css";
 
 function Gallery() {
   return (
     <section id="galeria">
       <h2>Galeria</h2>
-      <img src="/images/galeria1.jpg" alt="zdjecie salonu" />
-      <img src="/images/galeria2.jpg" alt="zdjecie salonu" />
-      <img src="/images/galeria3.jpg" alt="zdjecie salonu" />
-      <img src="/images/galeria4.jpg" alt="zdjecie salonu" />
-      <img src="/images/galeria2.jpg" alt="zdjecie salonu" />
+        <Splide 
+          options={{ 
+            type: "loop",
+            rewind:true,
+            perPage: 1,
+            focus  : 'center',
+            width: "100%",  
+            gap:"150px"}}>
+
+          <SplideSlide>
+        <img src="/images/galeria1.jpg" alt="zdjecie salonu" />
+        </SplideSlide>
+        <SplideSlide>
+        <img src="/images/galeria2.jpg" alt="zdjecie salonu" />
+        </SplideSlide>
+        <SplideSlide>
+        <img src="/images/galeria3.jpg" alt="zdjecie salonu" />
+        </SplideSlide>
+        <SplideSlide>
+        <img src="/images/galeria4.jpg" alt="zdjecie salonu" />
+        </SplideSlide>
+        <SplideSlide>
+        <img src="/images/galeria2.jpg" alt="zdjecie salonu" />
+        </SplideSlide>
+        </Splide>
     </section>
   );
 }
