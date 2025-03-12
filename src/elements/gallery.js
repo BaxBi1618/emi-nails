868f976d927eeb76a6ca9420f9e5a52bdc346037
Michał Garnys <m.programista@wp.pl>
2025-03-13 00:51:59 +0100

    ukończenie slidera używająć swiper-a

--- src/elements/gallery.js
+++ src/elements/gallery.js
@@ -1,25 +1,31 @@
 import "../styles/gallery.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+// import required modules
+import {Navigation } from 'swiper/modules';
 
 function Gallery() {
-  var settings = {
-    dots: false,
-    speed: 500,
-    slidesToShow:1,
-    slidesToScroll: 1,
-    infinite: true,
-    centerMode: true,
-  };
   return (
     <section id="galeria">
       <h2>Galeria</h2>
-        <Slider {...settings}>
-          <img src="/images/galeria1.jpg" alt="zdjecie salonu" />
-          <img src="/images/galeria2.jpg" alt="zdjecie salonu" />
-          <img src="/images/galeria3.jpg" alt="zdjecie salonu" />
-        </Slider>
+        <Swiper
+          slidesPerView={'2'}
+          centeredSlides={true}
+          spaceBetween={30}
+          navigation={true}
+          modules={[Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide><img src="/images/galeria1.jpg" alt="zdjecie salonu" /></SwiperSlide>
+          <SwiperSlide><img src="/images/galeria2.jpg" alt="zdjecie salonu" /></SwiperSlide>
+          <SwiperSlide><img src="/images/galeria3.jpg" alt="zdjecie salonu" /></SwiperSlide>
+          <SwiperSlide><img src="/images/galeria1.jpg" alt="zdjecie salonu" /></SwiperSlide>
+          <SwiperSlide><img src="/images/galeria2.jpg" alt="zdjecie salonu" /></SwiperSlide>
+          <SwiperSlide><img src="/images/galeria3.jpg" alt="zdjecie salonu" /></SwiperSlide>
+        </Swiper>
     </section>
   );
 }
