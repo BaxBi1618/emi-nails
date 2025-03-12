a09432fbd4d8b048278bb24b919470d52cec0b97
Michał Garnys <m.programista@wp.pl>
2025-03-13 00:55:14 +0100

    małe poprawki swipera

--- src/elements/gallery.js
+++ src/elements/gallery.js
@@ -1,24 +1,22 @@
 import "../styles/gallery.css";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
-import 'swiper/css/pagination';
 import 'swiper/css/navigation';
-
-// import required modules
 import {Navigation } from 'swiper/modules';
+
+const swiperOptions = {
+          slidesPerView: '2',
+          centeredSlides: true,
+          spaceBetween: 30,
+          navigation: true,
+          modules: [Navigation]
+}
 
 function Gallery() {
   return (
     <section id="galeria">
       <h2>Galeria</h2>
-        <Swiper
-          slidesPerView={'2'}
-          centeredSlides={true}
-          spaceBetween={30}
-          navigation={true}
-          modules={[Navigation]}
-          className="mySwiper"
-        >
+        <Swiper {...swiperOptions}>
           <SwiperSlide><img src="/images/galeria1.jpg" alt="zdjecie salonu" /></SwiperSlide>
           <SwiperSlide><img src="/images/galeria2.jpg" alt="zdjecie salonu" /></SwiperSlide>
           <SwiperSlide><img src="/images/galeria3.jpg" alt="zdjecie salonu" /></SwiperSlide>
